b6cb4e7466e5391d8baa36ac16bd64e440fa8f25
Leone Parise <leone.parise@gmail.com>
2017-02-21 09:57:33 -0300

    Update to Swift 3.0 and RxSwift 3.0

--- Sources/Methods.swift
+++ Sources/Methods.swift
@@ -2,105 +2,107 @@ import Firebase
 import RxSwift
 
 //Authentication Extension Methods
-public extension FQuery {
+public extension Reactive where Base: FQuery {
     /**
      - Returns: An `Observable<FAuthData?>`, `FAuthData?` will be nil if the user is logged out.
      */
-    var rx_authObservable :Observable<FAuthData?> {
+    var authObservable :Observable<FAuthData?> {
         get {
             return Observable.create({ (observer: AnyObserver<FAuthData?>) -> Disposable in
-                let ref = self.ref;
-                let listener = ref.observeAuthEventWithBlock({ (authData: FAuthData?) -> Void in
-                    observer.on(.Next(authData))
+                let ref = self.base.ref!;
+                let listener = ref.observeAuthEvent({ (authData: FAuthData?) -> Void in
+                    observer.on(.next(authData))
                 })
-                return AnonymousDisposable {
-                    self.ref.removeAuthEventObserverWithHandle(listener)
+                
+                return Disposables.create {
+                    self.base.ref.removeAuthEventObserver(withHandle: listener)
                 }
             })
         }
     }
     
-    func rx_authUser(email: String, password: String) -> Observable<FAuthData> {
-        let query = self;
+    func authUser(_ email: String, password: String) -> Observable<FAuthData> {
+        let query = self.base;
+        
         return Observable.create({ (observer: AnyObserver<FAuthData>) -> Disposable in
             query.ref.authUser(email, password: password, withCompletionBlock: { (error, authData) -> Void in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 }else{
-                    observer.on(.Next(authData))
-                    observer.on(.Completed)
+                    observer.on(.next(authData!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
     
-    func rx_authAnonymously() -> Observable<FAuthData> {
-        let query = self;
+    func authAnonymously() -> Observable<FAuthData> {
+        let query = self.base;
+        
         return Observable.create({ (observer: AnyObserver<FAuthData>) -> Disposable in
-            query.ref.authAnonymouslyWithCompletionBlock({ (error, authData) -> Void in
+            query.ref.authAnonymously(completionBlock: { (error, authData) -> Void in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 }else{
-                    observer.on(.Next(authData))
-                    observer.on(.Completed)
+                    observer.on(.next(authData!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_authWithOAuthProvider(provider: String, parameters: [NSObject: AnyObject]) -> Observable<FAuthData> {
-        let query = self;
+    func authWithOAuthProvider(_ provider: String, parameters: [AnyHashable: Any]) -> Observable<FAuthData> {
+        let query = self.base
         return Observable.create({ (observer: AnyObserver<FAuthData>) -> Disposable in
-            query.ref.authWithOAuthProvider(provider, parameters: parameters, withCompletionBlock: { (error, authData) -> Void in
+            query.ref.auth(withOAuthProvider: provider, parameters: parameters, withCompletionBlock: { (error, authData) -> Void in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 }else{
-                    observer.on(.Next(authData))
-                    observer.on(.Completed)
+                    observer.on(.next(authData!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
     
-    func rx_authWithOAuthProvider(provider: String, token: String) -> Observable<FAuthData> {
-        let query = self;
+    func authWithOAuthProvider(_ provider: String, token: String) -> Observable<FAuthData> {
+        let query = self.base
         return Observable.create({ (observer: AnyObserver<FAuthData>) -> Disposable in
-            query.ref.authWithOAuthProvider(provider, token: token, withCompletionBlock: { (error, authData) -> Void in
+            query.ref.auth(withOAuthProvider: provider, token: token, withCompletionBlock: { (error, authData) -> Void in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 }else{
-                    observer.on(.Next(authData))
-                    observer.on(.Completed)
+                    observer.on(.next(authData!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_authWithCustomToken(customToken: String) -> Observable<FAuthData> {
-        let query = self;
+    func authWithCustomToken(_ customToken: String) -> Observable<FAuthData> {
+        let query = self.base
         return Observable.create({ (observer: AnyObserver<FAuthData>) -> Disposable in
-            query.ref.authWithCustomToken(customToken, withCompletionBlock: { (error, authData) -> Void in
+            query.ref.auth(withCustomToken: customToken, withCompletionBlock: { (error, authData) -> Void in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 }else{
-                    observer.on(.Next(authData))
-                    observer.on(.Completed)
+                    observer.on(.next(authData!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_createUser(email: String, password: String) -> Observable<[NSObject: AnyObject]> {
-        let query = self;
-        return Observable.create { (observer : AnyObserver<[NSObject: AnyObject]>) -> Disposable in
-            
+    func createUser(_ email: String, password: String) -> Observable<[AnyHashable: Any]> {
+        let query = self.base
+        return Observable.create { (observer : AnyObserver<[AnyHashable: Any]>) -> Disposable in
             query.ref.createUser(email, password: password, withValueCompletionBlock: { (error, value) in
                 if let error = error {
                     observer.onError(error)
@@ -110,39 +112,38 @@ public extension FQuery {
                     observer.onCompleted()
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         }
     }
     
     
-    func rx_observe(eventType: FEventType) -> Observable<FDataSnapshot> {
-        let ref = self;
-        return Observable.create({ (observer : AnyObserver<FDataSnapshot>) -> Disposable in
-            let listener = ref.observeEventType(eventType, withBlock: { (snapshot) -> Void in
-                observer.on(.Next(snapshot))
-                }, withCancelBlock: { (error) -> Void in
-                    observer.on(.Error(error))
+    func observe(_ eventType: FEventType) -> Observable<FDataSnapshot> {
+        return Observable.create( { (observer : AnyObserver<FDataSnapshot>) -> Disposable in
+            let listener = self.base.ref.observe(eventType, with: { (snapshot) in
+                observer.on(.next(snapshot!))
+            }, withCancel: { (error) in
+                observer.on(.error(error!))
             })
-            return AnonymousDisposable{
-                ref.removeObserverWithHandle(listener)
+
+            return Disposables.create {
+                self.base.ref.removeObserver(withHandle: listener)
             }
         })
     }
     /**
      - Returns: A tuple `Observable<(FDataSnapshot, String)>` with the first value as the snapshot and the second value as the sibling key
      */
-    func rx_observeWithSiblingKey(eventType: FEventType) -> Observable<(FDataSnapshot, String?)> {
-        let ref = self;
-        
+    func observeWithSiblingKey(_ eventType: FEventType) -> Observable<(FDataSnapshot, String?)> {
         return Observable.create({ (observer : AnyObserver<(FDataSnapshot, String?)>) -> Disposable in
-            let listener = ref.observeEventType(eventType, andPreviousSiblingKeyWithBlock: { (snapshot, siblingKey) -> Void in
-                let tuple : (FDataSnapshot, String?) = (snapshot, siblingKey)
-                observer.on(.Next(tuple))
-                }, withCancelBlock: { (error) -> Void in
-                    observer.on(.Error(error))
+            let listener = self.base.ref.observe(eventType, andPreviousSiblingKeyWith: { (snapshot, siblingKey) in
+                let tuple : (FDataSnapshot, String?) = (snapshot!, siblingKey)
+                observer.on(.next(tuple))
+            }, withCancel: { (error) in
+                observer.on(.error(error!))
             })
-            return AnonymousDisposable{
-                ref.removeObserverWithHandle(listener)
+            
+            return Disposables.create {
+                self.base.ref.removeObserver(withHandle: listener)
             }
         })
     }
@@ -150,51 +151,49 @@ public extension FQuery {
     /**
      - Returns: The firebase reference where the update occured
      */
-    func rx_updateChildValues(values: [NSObject: AnyObject!]) -> Observable<Firebase> {
-        let query = self
+    func updateChildValues(_ values: [NSObject: AnyObject?]) -> Observable<Firebase> {
         return Observable.create({ (observer: AnyObserver<Firebase>) -> Disposable in
-            query.ref.updateChildValues(values, withCompletionBlock: { (error, firebase) -> Void in
+            self.base.ref.updateChildValues(values, withCompletionBlock: { (error, firebase) in
                 if let error = error {
-                    observer.on(.Error(error))
+                    observer.on(.error(error))
                 } else{
-                    observer.on(.Next(firebase))
-                    observer.on(.Completed)
+                    observer.on(.next(firebase!))
+                    observer.on(.completed)
                 }
             })
-            return NopDisposable.instance
+            
+            return Disposables.create()
         })
     }
     
-    func rx_setValue(value: AnyObject!, priority: AnyObject? = nil) -> Observable<Firebase> {
-        let query = self
+    func setValue(_ value: AnyObject!, priority: AnyObject? = nil) -> Observable<Firebase> {
         return Observable.create({ (observer: AnyObserver<Firebase>) -> Disposable in
             if let priority = priority {
-                query.ref.setValue(value, andPriority: priority, withCompletionBlock: { (error, firebase) -> Void in
+                self.base.ref.setValue(value, andPriority: priority, withCompletionBlock: { (error, firebase) -> Void in
                     if let error = error {
-                        observer.on(.Error(error))
+                        observer.on(.error(error))
                     } else{
-                        observer.on(.Next(firebase))
-                        observer.on(.Completed)
+                        observer.on(.next(firebase!))
+                        observer.on(.completed)
                     }
                 })
             }else {
-                query.ref.setValue(value, withCompletionBlock: { (error, firebase) -> Void in
+                self.base.ref.setValue(value, withCompletionBlock: { (error, firebase) -> Void in
                     if let error = error {
-                        observer.on(.Error(error))
+                        observer.on(.error(error))
                     } else{
-                        observer.on(.Next(firebase))
-                        observer.on(.Completed)
+                        observer.on(.next(firebase!))
+                        observer.on(.completed)
                     }
                 })
             }
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_removeValue() -> Observable<Firebase> {
-        let query = self
+    func removeValue() -> Observable<Firebase> {
         return Observable.create({ (observer: AnyObserver<Firebase>) -> Disposable in
-            query.ref.removeValueWithCompletionBlock({ (err, ref) -> Void in
+            self.base.ref.removeValue(completionBlock: { (err, ref) -> Void in
                 if let err = err {
                     observer.onError(err)
                 }else if let ref = ref {
@@ -202,14 +201,13 @@ public extension FQuery {
                     observer.onCompleted()
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_runTransactionBlock(block: ((FMutableData!) -> FTransactionResult!)!) -> Observable<(isCommitted: Bool, snapshot : FDataSnapshot)> {
-        let query = self
+    func runTransactionBlock(_ block: ((FMutableData?) -> FTransactionResult?)!) -> Observable<(isCommitted: Bool, snapshot : FDataSnapshot)> {
         return Observable.create({ (observer) -> Disposable in
-            query.ref.runTransactionBlock(block) { (err, isCommitted, snapshot) -> Void in
+            self.base.ref.runTransactionBlock(block) { (err, isCommitted, snapshot) -> Void in
                 if let err = err {
                     observer.onError(err)
                 } else if let snapshot = snapshot {
@@ -217,13 +215,13 @@ public extension FQuery {
                     observer.onCompleted()
                 }
             }
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_setPriority(priority : AnyObject) -> Observable<Firebase> {
+    func setPriority(_ priority : AnyObject) -> Observable<Firebase> {
         return Observable.create({ (observer) -> Disposable in
-            self.ref.setPriority(priority) { (error, ref) -> Void in
+            self.base.ref.setPriority(priority) { (error, ref) -> Void in
                 if let error = error {
                     observer.onError(error)
                 } else if let ref = ref {
@@ -232,14 +230,14 @@ public extension FQuery {
                 }
                 
             }
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_onDisconnectSetValue(value: AnyObject, priority: AnyObject? = nil) -> Observable<Firebase> {
+    func onDisconnectSetValue(_ value: AnyObject, priority: AnyObject? = nil) -> Observable<Firebase> {
         return Observable.create({ (observer) -> Disposable in
             if let priority = priority {
-                self.ref.onDisconnectSetValue(value, andPriority: priority, withCompletionBlock: { (error, ref) -> Void in
+                self.base.ref.onDisconnectSetValue(value, andPriority: priority, withCompletionBlock: { (error, ref) -> Void in
                     if let error = error {
                         observer.onError(error)
                     } else if let ref = ref {
@@ -247,9 +245,9 @@ public extension FQuery {
                         observer.onCompleted()
                     }
                 })
-                return NopDisposable.instance
+                return Disposables.create()
             } else {
-                self.ref.onDisconnectSetValue(value, withCompletionBlock: { (error, ref) -> Void in
+                self.base.ref.onDisconnectSetValue(value, withCompletionBlock: { (error, ref) -> Void in
                     if let error = error {
                         observer.onError(error)
                     } else if let ref = ref {
@@ -257,14 +255,14 @@ public extension FQuery {
                         observer.onCompleted()
                     }
                 })
-                return NopDisposable.instance
+                return Disposables.create()
             }
         })
     }
     
-    func rx_onDisconnectUpdateValue(values: [NSObject: AnyObject]) -> Observable<Firebase> {
+    func onDisconnectUpdateValue(_ values: [AnyHashable: Any]) -> Observable<Firebase> {
         return Observable.create({ (observer) -> Disposable in
-            self.ref.onDisconnectUpdateChildValues(values, withCompletionBlock: { (error, ref) -> Void in
+            self.base.ref.onDisconnectUpdateChildValues(values, withCompletionBlock: { (error, ref) -> Void in
                 if let error = error {
                     observer.onError(error)
                 } else if let ref = ref {
@@ -273,13 +271,13 @@ public extension FQuery {
                 }
             })
             
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
-    func rx_onDisconnectRemoveValueWithCompletionBlock() -> Observable<Firebase> {
+    func onDisconnectRemoveValueWithCompletionBlock() -> Observable<Firebase> {
         return Observable.create({ (observer) -> Disposable in
-            self.ref.onDisconnectRemoveValueWithCompletionBlock({ (error, ref) -> Void in
+            self.base.ref.onDisconnectRemoveValue(completionBlock: { (error, ref) -> Void in
                 if let error = error {
                     observer.onError(error)
                 } else if let ref = ref {
@@ -287,7 +285,7 @@ public extension FQuery {
                     observer.onCompleted()
                 }
             })
-            return NopDisposable.instance
+            return Disposables.create()
         })
     }
     
@@ -295,19 +293,17 @@ public extension FQuery {
 }
 
 public extension ObservableType where E : FDataSnapshot {
-    
-    func rx_filterWhenNSNull() -> Observable<E> {
+    func filterWhenNSNull() -> Observable<E> {
         return self.filter { (snapshot) -> Bool in
             return snapshot.value is NSNull
         }
     }
     
-    func rx_filterWhenNotNSNull() -> Observable<E> {
+    func filterWhenNotNSNull() -> Observable<E> {
         return self.filter { (snapshot) -> Bool in
             return !(snapshot.value is NSNull)
         }
     }
-    
 }
 
 
